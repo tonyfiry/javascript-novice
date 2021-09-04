7af95c472d928a448ed3e34ea7786179df606e54
tontfiry <tbo79435@gmail.com>
2021-09-04 23:55:51 +0800

    javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+let MyName = "我是張騏";//String字串;
+let num1 = 21;//Number數字;
+console.log(MyName+':'+num1+'歲');//顯示:我是張騏:21歲;
+
+let isFake = true;// true flase 布林值;
+console.log(isFake); //顯示:true;
+
+let coke = undefined;
+console.log(coke);
+
+let coke2 = null;
+console.log(coke2);
